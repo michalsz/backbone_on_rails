886f53f45e5997f112d208ad9f513bd1cfa68723
Michal <michal.t.szymanski@gmail.com>
2012-09-12 08:41:49 +0200

    Init recipes app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recipes::Application.config.secret_token = '5052aeca0212a02e3458571e4bce5f579d69fac19b232c9e88c4f1c2cc51c5970fa7322b6897f732c6e70dbfcb6fc3acf9700d013c89cafc0fc78c66d6e99f7c'
